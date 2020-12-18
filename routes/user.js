869ac14deeb35eb566a8f595a944064705783677
SavadMKD <savadmoorkkanad@gmail.com>
2020-12-18 15:09:55 +0530

    Created the login, signup page and printed the data on console

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//  Render Home Page
 router.get('/', function(req, res, next) {
   res.render('user/index');
 });
-
 // Render products page
 router.get('/products', (req, res) => {
-  res.render('user/products')
-})
+  res.render('user/products/products');
+});
+// Render Login and Signup Pages
+router.get('/signup', (req, res) => {
+  res.render('user/auth/signup');
+});
+router.get('/login', (req, res) => {
+  res.render('user/auth/login');
+});
+// Sending And Recieving Data of Login and Signup Page From Server
+router.post('/signup', (req, res) => {
+  console.log(req.body);
+});
+router.post('/login', (req, res) => {
+  console.log(req.body)
+});
 
 module.exports = router;
