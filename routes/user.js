15b003f1127c6b652f8cfbb192f5061827109660
SavadMKD <savadmoorkkanad@gmail.com>
2020-12-18 08:06:55 +0530

    Product Page Designed

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/index', { title: 'Express' });
+  res.render('user/index');
 });
+
+// Render products page
+router.get('/products', (req, res) => {
+  res.render('user/products')
+})
 
 module.exports = router;
