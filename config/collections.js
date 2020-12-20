151bb640deb4aee807cbf3fd081688a860038fff
SavadMKD <savadmoorkkanad@gmail.com>
2020-12-20 14:33:28 +0530

    Admin Login Created

--- config/collections.js
+++ config/collections.js
@@ -1,3 +1,4 @@
 module.exports = {
-    USER_COLLECTION: 'user'
+    USER_COLLECTION: 'user',
+    ADMIN_COLLECTION: 'admin'
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,48 @@
 var express = require('express');
+const authcontroller = require('../controller/authcontroller');
 var router = express.Router();
+
+// ============= Checking admin is logged in or not ==================
+const verifyLogin = (req, res, next) => {
+  if (req.session.adminLoggedIn) {
+    next();
+  } else {
+    res.redirect("/admin/login");
+  }
+};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let admin = req.session.admin;
+  res.render('user/index', { admin_page: true, admin });
+});
+// Rendering Admin_Login Page
+router.get("/login", (req, res) => {
+  if (req.session.adminLoggedIn) {
+    res.redirect("/");
+  } else {
+    res.render("admin/auth/login", { "adminloginErr": req.session.adminloginErr, admin_page:true });
+    req.session.adminloginErr = false;
+  }
+});
+// Login data connecting to the server
+router.post('/login', (req, res) => {
+  authcontroller.adminLogin(req.body).then((response) => {
+    if(response.status){
+      req.session.admin = response.admin;
+      req.session.adminLoggedIn = true;
+      res.redirect("/admin");
+    }
+    else{
+      req.session.adminloginErr = "Invlaid username or password";
+      res.redirect("/admin/login");
+    }
+  });
+});
+router.get('/logout', (req, res) => {
+  req.session.admin = null;
+  req.session.adminLoggedIn = false;
+  res.redirect("/admin/login");
 });
 
 module.exports = router;
